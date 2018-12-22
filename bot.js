a9e17bb7da5ed5025f6f69c17515c00a05a8f514
alirhaby <46032412+alirhaby@users.noreply.github.com>
2018-12-22 16:48:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -229,8 +229,8 @@ function play(guild, song) {
     serverQueue.textChannel.send(`بدء تشغيل : **${song.title}**`);
 }
  
-const adminprefix = "M";
-const devs = ['455288398249328650'];
+const adminprefix = "*";
+const devs = ['525346501388664833'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
